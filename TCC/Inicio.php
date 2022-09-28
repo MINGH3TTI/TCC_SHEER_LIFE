9870e1dbb3f0f8704491269e8fc2c0aa2e0e253c
MINGH3TTI <henriqueminghetti@gmail.com>
2022-09-28 11:54:29 -0300

    28-09

---
+++ TCC/Inicio.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/inicio.css">
+
+
+    
+    <title>Inicio</title>
+
+    <nav>
+          <img class="logo" src="img/sheer life.png">
+          <div class="mobile-menu">
+              <div class="line1"></div>
+              <div class="line2"></div>
+              <div class="line3"></div>
+          </div>
+          <ul class="nav-list">
+              <li><a href="Inicio.php">Início</a></li>
+              <li><a href="Sobre.php">Sobre</a></li>
+              <li><a href="Clinica.php">Clínicas</a></li>
+              <li><a href="Login.php">Login</a></li>
+          </ul>
+      </nav>
+      <br> <br>
+    
+
+
+    <script src="js/mobile-navbar.js"></script>
+    <br>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/empresa.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/empresa.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/empresa.png" alt="Third slide">
+    </div>
+  </div>
+</div>
+ 
+</body>
+
+
+
+</head>
+
+</html>
